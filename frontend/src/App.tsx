a84757dde3a64d8f8053a39cd0e4c6a512a49764
AliMoradpour <mr.mrdp@yahoo.com>
2026-04-29 12:49:27 +0330

    change backend codes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,14 @@
 function App() {
-  return <main className="bg-black text-white">ف.جامعه</main>;
+  return (
+    <main className="relative w-screen h-screen flex flex-col">
+      <div className="w-full h-full bg-green-500"></div>
+      <div className="w-full h-full bg-white"></div>
+      <div className="w-full h-full bg-red-500"></div>
+      <h1 className="font-black text-2xl absolute top-1/2 translate-x-80">
+        درود به مردم شریف ایران
+      </h1>
+    </main>
+  );
 }
 
 export default App;
