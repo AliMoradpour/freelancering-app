3218c5732c3d9ece83f6ef50192026c75fbc41f3
AliMoradpour <mr.mrdp@yahoo.com>
2026-04-29 14:44:11 +0330

    customize tailwindcss components classes

--- frontend/src/features/authentication/SendOTPForm.tsx
+++ frontend/src/features/authentication/SendOTPForm.tsx
@@ -4,7 +4,7 @@ type Props = {};
 
 const SendOTPForm = ({}: Props) => {
 
-  const [phoneNumber, setPhoneNumber] = useState<stringtring>('')
+  const [phoneNumber, setPhoneNumber] = useState<string>('')
 
   return (
     <div>
@@ -18,10 +18,10 @@ const SendOTPForm = ({}: Props) => {
             type="text"
             value={phoneNumber}
             onChange={(e)=> setPhoneNumber(e?.target?.value)}
-            className="w-full py-3 px-4 rounded-xl text-secondary-900 border border-gray-300 hover:border-primary-500 focus:border-primary-500 focus:bg-white focus:shadow-lg focus:shadow-primary-200 transition-all duration-300 ease-out"
+            className="textField__input"
           />
         </div>
-        <button className="px-4 py-2 font-bold bg-primary-900 text-white w-full rounded-xl hover:bg-primary-800 shadow-lg shadow-primary-300 transition-all duration-300">ارسال کد تایید</button>
+        <button className="btn btn--primary w-full">ارسال کد تایید</button>
       </form>
     </div>
   );
