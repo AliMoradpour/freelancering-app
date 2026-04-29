eec99d5274b3df0b67f7193ae4a55092359f8d57
AliMoradpour <mr.mrdp@yahoo.com>
2026-04-29 13:05:42 +0330

    check auth scenario

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+import Auth from "./components/Auth/Auth";
+import Home from "./Home";
+
 function App() {
   return (
-    <main className="relative w-screen h-screen flex flex-col">
-      <div className="w-full h-full bg-green-500"></div>
-      <div className="w-full h-full bg-white"></div>
-      <div className="w-full h-full bg-red-500"></div>
-      <h1 className="font-black text-2xl absolute top-1/2 translate-x-80">
-        درود به مردم شریف ایران
-      </h1>
-    </main>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/auth" element={<Auth />} />
+    </Routes>
   );
 }
 
---
+++ frontend/src/components/Auth/Auth.tsx
@@ -0,0 +1,12 @@
+type Props = {};
+
+export default function Auth({}: Props) {
+  return (
+    <div>
+      <h1>Auth</h1>
+      <a href="/" className="text-xs underline text-sky-600">
+        Go to Home Page
+      </a>
+    </div>
+  );
+}
