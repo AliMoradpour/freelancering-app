36ecd3a6ee2bbca94f9b7e95462b5232cd7c6e48
AliMoradpour <mr.mrdp@yahoo.com>
2026-04-29 13:09:51 +0330

    change folder structure into feature base

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,8 @@
 import { Route, Routes } from "react-router-dom";
-import Auth from "./components/Auth/Auth";
-import Home from "./Home";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/auth" element={<Auth />} />
     </Routes>
   );
 }
