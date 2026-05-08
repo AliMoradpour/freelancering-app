9060099f8e96d0e22138958f986b240b115dc6ff
AliMoradpour <mr.mrdp@yahoo.com>
2026-05-08 21:19:49 +0330

    modal markup

--- frontend/src/features/projects/ProjectRow.tsx
+++ frontend/src/features/projects/ProjectRow.tsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
 import type { owenrProjectType } from "../../types/projectTypes";
 import Table from "../../ui/Table";
 import toLocalDate from "../../utils/toLocalDate";
 import { toPersianNumbersWithComma } from "../../utils/toPersianNumbers";
 import truncateText from "../../utils/truncateText";
 
-function ProjectRow({project, index}: {project: owenrProjectType, index: number}) {
+import { HiOutlineTrash } from "react-icons/hi";
+import { TbPencilMinus } from "react-icons/tb";
+import Modal from "../../ui/Modal";
+
+function ProjectRow({
+  project,
+  index,
+}: {
+  project: owenrProjectType;
+  index: number;
+}) {
+  const [isEditOpen, setIsEditOpen] = useState<boolean>(false);
+  const [isDeleteOpen, setIsDeleteOpen] = useState<boolean>(false);
+
   return (
     <Table.Row key={project._id}>
       <td>{index + 1}</td>
@@ -29,7 +43,32 @@ function ProjectRow({project, index}: {project: owenrProjectType, index: number}
           <span className="badge badge--danger">بسته</span>
         )}
       </td>
-      <td>---</td>
+      <td>
+        <div className="flex items-center gap-x-4">
+          {/* Edit  */}
+          <button onClick={() => setIsEditOpen(true)}>
+            <TbPencilMinus className="w-5 h-5 text-primary-900" />
+          </button>
+          <Modal
+            title="Edit Content"
+            open={isEditOpen}
+            onClose={() => setIsEditOpen(false)}
+          >
+            This modal is for editing purposess
+          </Modal>
+          {/* Delete */}
+          <button onClick={() => setIsDeleteOpen(true)}>
+          <HiOutlineTrash className="w-5 h-5 text-error" />
+          </button>
+          <Modal
+            title="Delete Content"
+            open={isDeleteOpen}
+            onClose={() => setIsDeleteOpen(false)}
+          >
+            This modal is for Deleting purposess
+          </Modal>
+        </div>
+      </td>
     </Table.Row>
   );
 }
