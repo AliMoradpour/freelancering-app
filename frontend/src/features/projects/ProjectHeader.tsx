360e256f4c8e460025851417d1b17dea2297ab54
AliMoradpour <mr.mrdp@yahoo.com>
2026-05-09 11:21:39 +0330

    add new project button and project header

---
+++ frontend/src/features/projects/ProjectHeader.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import Modal from "../../ui/Modal";
+import CreateProject from "./CreateProject";
+
+function ProjectHeader() {
+ const [isAddingProject, setIsAddingProject] = useState<boolean>(false)
+  return (
+    <div className="flex items-center justify-between">
+      <h4 className="font-bold text-secondary-800">پروژه های شما</h4>
+      <button className="btn btn--primary" onClick={()=> setIsAddingProject(true)}>
+       پروژه جدید
+      </button>
+      <Modal title="اضافه کردن پروژه جدید" open={isAddingProject} onClose={()=> setIsAddingProject(false)}>
+       <CreateProject />
+      </Modal>
+    </div>
+  );
+}
+
+export default ProjectHeader;
--- frontend/src/pages/Projects.tsx
+++ frontend/src/pages/Projects.tsx
@@ -1,8 +1,10 @@
+import ProjectHeader from "../features/projects/ProjectHeader";
 import ProjectTable from "../features/projects/ProjectTable";
 
 function Projects() {
   return (
-    <div>
+    <div className="space-y-10">
+      <ProjectHeader />
       <ProjectTable />
     </div>
   );
