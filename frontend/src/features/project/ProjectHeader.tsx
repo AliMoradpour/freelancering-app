29c1b484e219e616d33ba24a557fcf50f8a32ca9
AliMoradpour <mr.mrdp@yahoo.com>
2026-06-26 10:18:12 +0330

    project header

--- frontend/src/features/project/ProjectHeader.tsx
+++ frontend/src/features/project/ProjectHeader.tsx
@@ -1,5 +1,15 @@
+import { HiArrowRight } from "react-icons/hi";
 import type { ownerProjectType } from "../../types/projectTypes";
+import useMoveBack from "../../hooks/useMoveBack";
 
 export default function ProjectHeader({ project }: { project: ownerProjectType }) {
-  return <div>{project.title}</div>;
+  const moveBack = useMoveBack();
+  return (
+    <div className="flex items-center gap-x-4 mb-8">
+      <button onClick={moveBack}>
+        <HiArrowRight className="size-5 text-secondary-500" />
+      </button>
+      <h1 className="font-black text-secondary-700 text-xl">لیست درخواست های {project.title}</h1>
+    </div>
+  );
 }
